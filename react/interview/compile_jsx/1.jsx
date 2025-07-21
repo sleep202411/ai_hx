497f56cea5e6159703f7a6869085b91edadcedce
sleep202411 <3247716708@qq.com>
2025-07-21 11:01:54 +0800

    babel

---
+++ react/interview/compile_jsx/1.jsx
@@ -0,0 +1,8 @@
+const element = <h1>Hello,world</h1>;
+const element2 = (
+    <ul>
+        <li key="q1">1</li>
+        <li key="a2">2</li>
+        <li key="z3">3</li>
+    </ul>
+)
---
+++ react/interview/compile_jsx/2.jsx
@@ -0,0 +1,8 @@
+const element = /*#__PURE__*/React.createElement("h1", null, "Hello,world");
+const element2 = /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", {
+  key: "q1"
+}, "1"), /*#__PURE__*/React.createElement("li", {
+  key: "a2"
+}, "2"), /*#__PURE__*/React.createElement("li", {
+  key: "z3"
+}, "3"));
